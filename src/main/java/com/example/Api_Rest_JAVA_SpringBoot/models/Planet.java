ddc275bb53f9d0ed195dae978750e9f38868d13b
freimyHidalgo77 <freimy4@gmail.com>
2026-02-23 11:18:45 -0400

    Entidad planeta con sus atributos

--- src/main/java/com/example/Api_Rest_JAVA_SpringBoot/models/Planet.java
+++ src/main/java/com/example/Api_Rest_JAVA_SpringBoot/models/Planet.java
@@ -1,4 +1,31 @@
 package com.example.Api_Rest_JAVA_SpringBoot.models;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Planets")
 public class Planet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String nombre;
+    private String Rocoso;
+    private double masa;
+    private int radio;
+    private double gravedad;
+    private double distancia_al_sol;
+    private int periodo_orbital;
+    private int numero_de_lunas;
+    private boolean tiene_anillos = false;
+
+
 }
+
